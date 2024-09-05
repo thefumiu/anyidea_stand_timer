bed750bf25e2d8598ef5ece5f39c25bba0176272
thefumiu <Uysalfurkanmirac@icloud.com>
2024-09-05 20:07:02 +0300

    Server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+app.use(express.static('files'))
+const server = createServer(app);
+const io = new Server(server);
+
+var ONLINE_USERS = 0;
+
+io.on('connection', (socket) => {
+    ONLINE_USERS++;
+    io.emit("value ONLINE_USERS", ONLINE_USERS);
+
+    socket.on('disconnect', (socket) => {
+        ONLINE_USERS--;
+        io.emit("value ONLINE_USERS", ONLINE_USERS);
+    });
+});
+
+// GET METHODS
+app.get('/timer', (req, res) => {
+    res.render('timer.html');
+});
+
+server.listen(8082, () => {
+    console.log('server running at http://localhost:8082');
+});
